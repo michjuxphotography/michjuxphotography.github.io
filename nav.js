c67bb1ff580054d5853d541aa1ed4c00b31bb8cd
MichjuX <michjuxphotography@gmail.com>
2024-09-01 03:30:13 +0200

    small animation timing changes

--- nav.js
+++ nav.js
@@ -10,9 +10,9 @@ const navSlide = () => {
         // Animate links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = ''; // Reset animation
+                link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+0.2}s`;
             }
         });
 
